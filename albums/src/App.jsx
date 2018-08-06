e9bc26daaddd7a4c6a7a0f676329a1e64953f389
Mark Wen <markwenguojie94@gmail.com>
2018-08-06 14:55:35 -0400

    Added album list fetching capabilities

--- albums/src/App.jsx
+++ albums/src/App.jsx
@@ -1,31 +1,17 @@
 import React from 'react'
 import {
-  Platform,
-  Text,
   View,
 } from 'react-native'
 
 import Header from './components/Header'
+import AlbumList from './components/AlbumList'
 
 import styles from './styles/App.style'
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\nCmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\nShake or press menu button for dev menu',
-})
 
 const App = () => (
   <View style={styles.container}>
     <Header name="Albums" />
-    <Text style={styles.welcome}>
-      Welcome to React Native!
-    </Text>
-    <Text style={styles.instructions}>
-      To get started, edit App.js
-    </Text>
-    <Text style={styles.instructions}>
-      {instructions}
-    </Text>
+    <AlbumList />
   </View>
 )
 
---
+++ albums/src/components/AlbumList/AlbumList.jsx
@@ -0,0 +1,75 @@
+import React, { Component } from 'react'
+import {
+  Text,
+  View,
+} from 'react-native'
+import axios from 'axios'
+
+import styles from './style'
+
+
+class AlbumList extends Component {
+  state = {
+    list: [],
+    loading: false,
+    error: null,
+  }
+
+  componentDidMount() {
+    this.setState({ loading: true }, () => {
+      axios.get('https://rallycoding.herokuapp.com/api/music_albums')
+        .then((res) => {
+          this.setState({ list: res.data, loading: false })
+        })
+        .catch((error) => {
+          this.setState({ list: error.response, loading: false })
+        })
+    })
+  }
+
+  renderAlbum = this.renderAlbum.bind(this)
+
+  renderAlbum() {
+    const { list, loading } = this.state
+
+    if (loading) {
+      return (
+        <Text>
+          Loading...
+        </Text>
+      )
+    } if (!list || list.length <= 0) {
+      return (
+        <Text>
+          No albums are fetched
+        </Text>
+      )
+    }
+
+    return list.map(album => (
+      <Text key={album.url}>
+        {album.title}
+      </Text>
+    ))
+  }
+
+  render() {
+    const { error } = this.state
+
+    return (
+      <View style={styles.listWrapper}>
+        {
+          !error
+            ? this.renderAlbum()
+            : (
+              <Text>
+                Something wrong happened...
+              </Text>
+            )
+        }
+      </View>
+    )
+  }
+}
+
+export default AlbumList
